e4d33c4e40081bf6f1ca40f6706d2755ed874472
noaccessl <54954576+noaccessl@users.noreply.github.com>
2023-06-03 15:33:28 +0300

    fix

--- visualcharacterheight.lua
+++ visualcharacterheight.lua
@@ -1,29 +1,21 @@
 do
 
-	local SetFont = surface.SetFont
-
-	local GetTextSize = surface.GetTextSize
 	local GetRenderTargetEx = GetRenderTargetEx
-
 	local PushRenderTarget = render.PushRenderTarget
+	local PopRenderTarget = render.PopRenderTarget
+	local CapturePixels = render.CapturePixels
+	local ReadPixel = render.ReadPixel
 
-	local Start2D = cam.Start2D
-
+	local SetFont = surface.SetFont
+	local GetTextSize = surface.GetTextSize
 	local SetDrawColor = surface.SetDrawColor
 	local DrawRect = surface.DrawRect
-
 	local SetTextPos = surface.SetTextPos
 	local SetTextColor = surface.SetTextColor
 	local DrawText = surface.DrawText
 
+	local Start2D = cam.Start2D
 	local End2D = cam.End2D
-
-	local PopRenderTarget = render.PopRenderTarget
-
-	local SetRenderTarget = render.SetRenderTarget
-	local CapturePixels = render.CapturePixels
-
-	local ReadPixel = render.ReadPixel
 
 	local min = math.min
 	local max = math.max
@@ -52,30 +44,29 @@ do
 
 			End2D()
 
-		PopRenderTarget()
+			CapturePixels()
 
-		SetRenderTarget( rt )
-		CapturePixels()
+			local min_y = h
+			local max_y = 0
 
-		local min_y = h
-		local max_y = 0
+			for y = 0, h - 1 do
 
-		for y = 0, h - 1 do
+				for x = 0, w - 1 do
 
-			for x = 0, w - 1 do
+					local r, g, b = ReadPixel( x, y )
 
-				local r, g, b = ReadPixel( x, y )
+					if r > 0 and g > 0 and b > 0 then
 
-				if r > 0 and g > 0 and b > 0 then
+						min_y = min( min_y, y )
+						max_y = max( max_y, y )
 
-					min_y = min( min_y, y )
-					max_y = max( max_y, y )
+					end
 
 				end
 
 			end
 
-		end
+		PopRenderTarget()
 
 		max_y = max_y + 1
 
